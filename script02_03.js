270b856c4940d4dd1da5876cfca956a0a510af18
Gino Gustavo <ing.gino@gmail.com>
2020-09-28 01:08:53 -0500

    drawing with svg

---
+++ script02_03.js
@@ -0,0 +1,22 @@
+d3.select("#viz")
+    .append('svg')
+        .attr('width',600)
+        .attr('height',400)
+        .style('background', '#93A1A1')
+    .append('rect')
+        .attr('x',200)
+        .attr('y',100)
+        .attr('height',200)
+        .attr('width',200)
+        .style('fill', 'red');
+
+d3.select('#viz svg')
+    .append('circle')
+    .attr('cx',300)
+    .attr('cy',200)
+    .attr('r',50)
+    .style('fill', 'blue')
+    
+
+
+        
